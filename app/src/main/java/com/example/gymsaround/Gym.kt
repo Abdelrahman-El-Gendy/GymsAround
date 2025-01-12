1fe537c12b50281c651e8a355818747c2d996bc2
Abdelrahman Elgendy <abdelrahmanelgendy961@gmail.com>
2025-01-12 17:46:20 +0200

    Hoisting to ViewModel

--- app/src/main/java/com/example/gymsaround/Gym.kt
+++ app/src/main/java/com/example/gymsaround/Gym.kt
@@ -1,26 +1,37 @@
 package com.example.gymsaround
 
 data class Gym(
+    var id: Int,
     val name: String,
-    val address: String
+    val address: String,
+    var isFavourite: Boolean = false
 )
 
 val listOfGyms = listOf<Gym>(
     Gym(
+        1,
         name = "UpTown Gym",
-        address = "20 El_Gihad, Mit Akaba, Agoza, Gize Governorate 3745125, Egypt"
+        address = "20 El-Gihad, Mit Akaba, Agoza, Gize Governorate 3745125, Egypt"
     ),
     Gym(
-        name = "UpTown Gym",
-        address = "20 El_Gihad, Mit Akaba, Agoza, Gize Governorate 3745125, Egypt"
+        2,
+        name = "Gold's Gym",
+        address = "61-Syria, Mit Akaba, Agoza, Gize Governorate 3745125, Egypt"
     ),
     Gym(
-        name = "UpTown Gym",
-        address = "20 El_Gihad, Mit Akaba, Agoza, Gize Governorate 3745125, Egypt"
+        3,
+        name = "Hammer Gym",
+        address = "7-sphinx Square, Al Huwaiytagyah, Agoza, Gize Governorate 3745125, Egypt"
     ),
     Gym(
-        name = "UpTown Gym",
-        address = "20 El_Gihad, Mit Akaba, Agoza, Gize Governorate 3745125, Egypt"
+        4,
+        name = "I-Energy Gym",
+        address = "22-Gool Gamal, Al Huwaiytagyah, Agoza, Gize Governorate 3745125, Egypt"
+    ),
+    Gym(
+        5,
+        name = "H2o Gym & Spa",
+        address = "32-Anas Ibrahim Malek, Mit Akaba, Agoza, Gize Governorate 3745125, Egypt"
     )
 
 )
--- app/src/main/java/com/example/gymsaround/GymsViewModel.kt
+++ app/src/main/java/com/example/gymsaround/GymsViewModel.kt
@@ -1,7 +1,21 @@
 package com.example.gymsaround
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
-class GymsViewModel :ViewModel(){
-    fun getGyms() = listOfGyms
+class GymsViewModel : ViewModel() {
+
+    var state by mutableStateOf(getGyms())
+
+    private fun getGyms() = listOfGyms
+
+    fun toggleFavouriteState(gymId: Int) {
+        val gyms = state.toMutableList()
+        val indexItem = gyms.indexOfFirst { it.id == gymId }
+        gyms[indexItem] = gyms[indexItem].copy(isFavourite = !gyms[indexItem].isFavourite)
+        state = gyms
+    }
+
 }
